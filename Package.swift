3fa64ea9111912831c250e58d5650f843c0c67e9
Daniel Lyons <72824209+DandyLyons@users.noreply.github.com>
2025-10-04 14:53:04 -0600

    Require macOS 11 OSLog Logger
    We require macOS 11 and up now so that we can support

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FrontRange",
     platforms: [
-      .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13),
+      .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7), .macCatalyst(.v14),
     ],
     products: [
         .library(
