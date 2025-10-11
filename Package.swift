bd4e25207fba6a99aa1f7b566521b809be127af4
Daniel Lyons <72824209+DandyLyons@users.noreply.github.com>
2025-10-10 20:22:25 -0600

    Clean up Package.swift indentation

--- Package.swift
+++ Package.swift
@@ -4,75 +4,73 @@
 import PackageDescription
 
 let package = Package(
-    name: "FrontRange",
-    platforms: [
-      .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7), .macCatalyst(.v14),
-    ],
-    products: [
-        .library(
-            name: "FrontRange",
-            targets: ["FrontRange"],
-        ),
-        .library(
-            name: "FrontRangeCLICore",
-            targets: ["FrontRangeCLICore"]
-        ),
-        .executable(
-          name: "fr",
-          targets: ["FrontRangeCLI"]
-        ),
-    ],
-    dependencies: [
-      .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1"), // ArgumentParser
-      .package(url: "https://github.com/tuist/Command.git", from: "0.13.0"), // Command
-      .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "1.3.3"), // CustomDump
-      .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.14.1"), // Parsing
-      .package(url: "https://github.com/kylef/PathKit", from: "1.0.1"), // PathKit
-      .package(url: "https://github.com/jpsim/Yams.git", from: "6.1.0"), // Yams
-    ],
-    targets: [
-        .target(
-// A tool for parsing, mutating, serializing, and deserializing text documents with YAML front matter.
-            name: "FrontRange",
-            dependencies: [
-              .product(name: "CustomDump", package: "swift-custom-dump"),
-              .product(name: "Parsing", package: "swift-parsing"),
-              "Yams",
-            ],
-        ),
-        .testTarget(
-            name: "FrontRangeTests",
-            dependencies: [
-              .product(name: "CustomDump", package: "swift-custom-dump"),
-              "FrontRange",
-            ]
-        ),
-        
-        .target(
-// A command-line interface (CLI) for interacting with FrontRange functionalities.
-            name: "FrontRangeCLICore",
-            dependencies: [
-              "FrontRange",
-              .product(name: "ArgumentParser", package: "swift-argument-parser"),
-              .product(name: "PathKit", package: "PathKit"),
-            ],
-        ),
-        .testTarget(
-            name: "FrontRangeCLICoreTests",
-            dependencies: [
-              .product(name: "Command", package: "command"),
-              .target(name: "FrontRangeCLICore"),
-            ],
-            resources: [
-              .copy("../../ExampleFiles"),
-            ],
-        ),
-        
-          .executableTarget(
-            name: "FrontRangeCLI",
-            dependencies: [
-              .target(name: "FrontRangeCLICore"),
-            ],
-          ),
-    ]
+  name: "FrontRange",
+  platforms: [
+    .macOS(.v11), .iOS(.v14), .tvOS(.v14), .watchOS(.v7), .macCatalyst(.v14),
+  ],
+  products: [
+    .library(
+      name: "FrontRange",
+      targets: ["FrontRange"],
+    ),
+    .library(
+      name: "FrontRangeCLICore",
+      targets: ["FrontRangeCLICore"]
+    ),
+    .executable(
+      name: "fr",
+      targets: ["FrontRangeCLI"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1"), // ArgumentParser
+    .package(url: "https://github.com/tuist/Command.git", from: "0.13.0"), // Command
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "1.3.3"), // CustomDump
+    .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.14.1"), // Parsing
+    .package(url: "https://github.com/kylef/PathKit", from: "1.0.1"), // PathKit
+    .package(url: "https://github.com/jpsim/Yams.git", from: "6.1.0"), // Yams
+  ],
+  targets: [
+    .target(
+      // A tool for parsing, mutating, serializing, and deserializing text documents with YAML front matter.
+      name: "FrontRange",
+      dependencies: [
+        .product(name: "CustomDump", package: "swift-custom-dump"),
+        .product(name: "Parsing", package: "swift-parsing"),
+        "Yams",
+      ],
+    ),
+    .testTarget(
+      name: "FrontRangeTests",
+      dependencies: [
+        .product(name: "CustomDump", package: "swift-custom-dump"),
+        "FrontRange",
+      ]
+    ),
+    .target(
+      // A command-line interface (CLI) for interacting with FrontRange functionalities.
+      name: "FrontRangeCLICore",
+      dependencies: [
+        "FrontRange",
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "PathKit", package: "PathKit"),
+      ],
+    ),
+    .testTarget(
+      name: "FrontRangeCLICoreTests",
+      dependencies: [
+        .product(name: "Command", package: "command"),
+        .target(name: "FrontRangeCLICore"),
+      ],
+      resources: [
+        .copy("../../ExampleFiles"),
+      ],
+    ),
+    .executableTarget(
+      name: "FrontRangeCLI",
+      dependencies: [
+        .target(name: "FrontRangeCLICore"),
+      ],
+    ),
+  ]
 )
