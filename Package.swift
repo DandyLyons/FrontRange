3e01eba21e872a4c02fc9927b966a6e3a83d3b6d
Daniel Lyons <72824209+DandyLyons@users.noreply.github.com>
2025-12-06 17:15:18 -0700

    Add JMESPath dependency for search functionality
    - Add jmespath.swift package dependency
    - Supports JMESPath query language for filtering front matter
    - Foundation for implementing search command

--- Package.swift
+++ Package.swift
@@ -26,6 +26,7 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1"), // ArgumentParser
     .package(url: "https://github.com/tuist/Command.git", from: "0.13.0"), // Command
     .package(url: "https://github.com/pointfreeco/swift-custom-dump.git", from: "1.3.3"), // CustomDump
+    .package(url: "https://github.com/adam-fowler/jmespath.swift.git", from: "1.0.3"), // JMESPath
     .package(url: "https://github.com/modelcontextprotocol/swift-sdk", from: "0.7.1"), // ModelContextProtocol Swift SDK
     .package(url: "https://github.com/pointfreeco/swift-parsing.git", from: "0.14.1"), // Parsing
     .package(url: "https://github.com/kylef/PathKit", from: "1.0.1"), // PathKit
@@ -58,6 +59,7 @@ let package = Package(
       dependencies: [
         "FrontRange",
         .product(name: "ArgumentParser", package: "swift-argument-parser"), // CLI argument parsing
+        .product(name: "JMESPath", package: "jmespath.swift"), // JMESPath query support
         .product(name: "PathKit", package: "PathKit"), // For file path handling
       ],
     ),
